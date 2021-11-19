4f091756cc7701f94c7aff5c59ade7b6c0063d45
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-11-19 15:14:17 -0500

    deleted "text-6xl"

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ children }) {
   return (
-    <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl md:text-5xl md:leading-14">
+    <h1 className="font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl md:text-5xl md:leading-14">
       {children}
     </h1>
   )
