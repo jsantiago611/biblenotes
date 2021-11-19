1264908a7c9daab283889b86b10e144ecc360042
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-11-19 14:29:13 -0500

    enlarging the page titles
    With tailwind.

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ children }) {
   return (
-    <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl sm:leading-10 md:text-6xl md:leading-14">
       {children}
     </h1>
   )
