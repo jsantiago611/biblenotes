5fb4d744512a37a0f1f2192365de919fea767b74
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-11-21 00:14:22 -0500

    Update PageTitle.js

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ children }) {
   return (
-    <h1 className="font-ogg text-3xl font-extrabold leading-9 tracking-normal text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <h1 className="font-ogg text-3xl font-normal leading-9 tracking-normal text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       {children}
     </h1>
   )
