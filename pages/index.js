c8c836aa9e84baa85dfe4e0d051670827fd745f2
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-12-08 14:05:51 -0500

    updated font fam from monument to bradford

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="font-monument text-3xl font-normal leading-9 tracking-normal text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="font-bradford text-3xl font-normal leading-9 tracking-normal text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
@@ -45,7 +45,7 @@ export default function Home({ posts }) {
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="font-monument text-2xl font-normal leading-8 tracking-normal">
+                          <h2 className="font-bradford text-2xl font-normal leading-8 tracking-normal">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
