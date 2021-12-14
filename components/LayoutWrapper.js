2caac7e20e33dd114db2cb8d305c33a990574371
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-12-14 10:42:46 -0500

    updated site title to Windsor

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -19,7 +19,7 @@ const LayoutWrapper = ({ children }) => {
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:contents">
+                  <div className="font-windsor hidden h-6 text-2xl font-bold sm:contents">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
