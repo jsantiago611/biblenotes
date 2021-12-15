0d2b0b59db96e9c1bf7d6a9e796c9c27305dada9
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-12-15 09:16:11 -0500

    increased padding around hero
    line23

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <div className="pt-8 pb-20 space-y-2 md:space-y-5">
           <h1 className="font-windsor text-3xl font-bold leading-9 tracking-[.5px] text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             The Parent's Progress
           </h1>
