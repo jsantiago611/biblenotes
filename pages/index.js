746bdc4f526622c0de40cd542fb94f2cebe06bf1
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-12-15 15:01:04 -0500

    published on date to a lighter gray

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home({ posts }) {
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
                     <dl>
                       <dt className="sr-only">Published on</dt>
-                      <dd className="font-gt text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                      <dd className="font-gt text-base font-medium leading-6 text-gray-400 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
                     </dl>
