342f681eb6bbe23593f5258c68ef5bbd3a262a44
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-12-15 09:25:33 -0500

    changed page title to windsor, page title text

--- pages/projects.js
+++ pages/projects.js
@@ -9,11 +9,11 @@ export default function Projects() {
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
+          <h1 className="font-windsor text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Summaries
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Browse through this collection of time-saving sermon and book summaries to quickly boost your spiritual growth. 
           </p>
         </div>
         <div className="container py-12">
